ac0bc40893d6218523556d54b1f0116029a97b05
girikgarg8 <girikgarg8@gmail.com>
2023-01-29 14:38:38 +0530

    4:29:48

---
+++ com/girik/Package.java
@@ -0,0 +1,8 @@
+package com.girik;
+import com.girik.test.Student;
+public class Package {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.rollno=100;
+    }
+}
---
+++ com/girik/test/Student.java
@@ -0,0 +1,8 @@
+package com.girik.test;
+
+public class Student {
+    public Student(){
+        System.out.println (" Hello, I am a student ");
+    }
+    public int rollno=8;
+}
