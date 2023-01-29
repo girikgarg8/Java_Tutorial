d5c8061deac5b25c35cb1185bdb40127b3dda044
girikgarg8 <girikgarg8@gmail.com>
2023-01-30 01:01:15 +0530

    4:46:19

--- com/girik/ExceptionDemo.java
+++ com/girik/ExceptionDemo.java
@@ -7,8 +7,14 @@ public class ExceptionDemo{
             int k=i/j;
             System.out.println("output is "+k);
         }
+        catch(ArithmeticException e){
+            System.out.println( " Cannot divide by your zero  ");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println (" Array index out of bound exception "); 
+        }
         catch(Exception e){
-            System.out.println( " Error ");
+            System.out.println(" Some other exception occured ");
         }
         finally{
             System.out.println (" Bye ");
