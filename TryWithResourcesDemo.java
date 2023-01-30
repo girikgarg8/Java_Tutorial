c33b3628222c37b539e83e5a0689e73c3064ee8f
girikgarg8 <girikgarg8@gmail.com>
2023-01-30 10:38:28 +0530

    4:50:34

---
+++ TryWithResourcesDemo.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class TryWithResourcesDemo {
+    public static void main(String[] args) {
+        int n=0;
+        System.out.println(" Enter a number ");
+        try(BufferedReader br=new BufferedReader(System.in)){
+            
+        }
+    }
+}
