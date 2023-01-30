f4ba4a3e596fd340a14a528ff6f181a82daaa5aa
girikgarg8 <girikgarg8@gmail.com>
2023-01-30 10:46:31 +0530

    4:51:06

--- TryWithResourcesDemo.java
+++ TryWithResourcesDemo.java
@@ -4,8 +4,12 @@ public class TryWithResourcesDemo {
     public static void main(String[] args) {
         int n=0;
         System.out.println(" Enter a number ");
-        try(BufferedReader br=new BufferedReader(System.in)){
-            
+        try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in))){
+            n=Integer.parseInt(br.readLine());
+        }
+        finally{
+            br.close();
+            System.out.println("closed");
         }
     }
 }
